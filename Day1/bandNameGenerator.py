787b11cbd6c07c0b6db13cad11bad2fd7496458f
Sparky-turned-Coder <134346433+Sparky-turned-Coder@users.noreply.github.com>
2025-12-13 14:23:09 -0600

    completed day 1

---
+++ Day1/bandNameGenerator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator!")
+
+city = input("What is the city you grew up in? ").capitalize()
+pet = input("What is your favorite pet's name? ").capitalize()
+
+print(f"Your band name could be {city} {pet}!")
